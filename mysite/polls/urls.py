96011d11b351349953e87d02414f31c1afafa9eb
Thiago Colares <thicolares@gmail.com>
2017-01-07 21:23:06 -0200

    Descopla o link do template com a definição em polls.urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5
+    # the 'name' value as called by the {% url %} template tag
+    # If you want to change the URL of the polls detail view to something else, perhaps to something like polls/specifics/12/ instead of doing it in the template (or templates) you would change it in polls/urls.py:
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
