42a152915d0ca39127a2878f4b5bd7695d562593
Thiago Colares <thicolares@gmail.com>
2017-01-12 22:10:22 -0200

    Faz o teste passar

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,7 +7,7 @@ from .models import Question
 
 
 class QuestionMethodTests(TestCase):
-
+    # it looked for test methods - ones whose names begin with test
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() should return False for questions whose
