156ce52692eac35763c1bca757d117da6c0f683e
Thiago Colares <thicolares@gmail.com>
2017-01-12 22:07:22 -0200

    Volta para o estado original de Question

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,6 +9,10 @@ from .models import Question
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        future_time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_time)
         self.assertIs(future_question.was_published_recently(), False)
