aa66f57e319f9d6e009689191329ebdb70b5878d
Thiago Colares <thicolares@gmail.com>
2017-01-14 17:43:12 -0200

    Testa se página de detalhes de uma Question futura retorna 404 e falha

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,6 +6,18 @@ from django.urls import reverse
 
 from .models import Question
 
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date 
+        in the future should return a 404 not found
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
 def create_question(question_text, days):
     """
     Creates a question with the givent question_text
